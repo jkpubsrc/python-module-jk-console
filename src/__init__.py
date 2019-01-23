c2b725c58d3b010052a50c9f9ae4f24430194808
Jürgen Knauth <jknauth@uni-goettingen.de>
2019-01-23 07:40:33 +0100

    Fixing missing BOM and adding version string

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,9 @@
 
 from .jk_console import *
 
+
+
+__version__ = "0.2019.1.22"
+
+
+
--- src/setup.py
+++ src/setup.py
@@ -1,35 +1,35 @@
-from setuptools import setup
+﻿from setuptools import setup
 
 
 def readme():
-	with open('README.rst') as f:
+	with open("README.rst") as f:
 		return f.read()
 
 
-setup(name='jk_console',
-	version='0.2018.4.10',
-	description='This python module provides a variety of essential functionality for implementing versatile programs using the console. (Please have a look at the documentation for details.)',
-	author='Jürgen Knauth',
-	author_email='pubsrc@binary-overflow.de',
-	license='Apache 2.0',
-	url='https://github.com/jkpubsrc/python-module-jk-console',
-	download_url='https://github.com/jkpubsrc/python-module-jk-json/tarball/0.2018.4.10',
+setup(name="jk_console",
+	version="0.2019.1.22",
+	description="This python module provides a variety of essential functionality for implementing versatile programs using the console. (Please have a look at the documentation for details.)",
+	author="Jürgen Knauth",
+	author_email="pubsrc@binary-overflow.de",
+	license="Apache 2.0",
+	url="https://github.com/jkpubsrc/python-module-jk-console",
+	download_url="https://github.com/jkpubsrc/python-module-jk-json/tarball/0.2019.1.22",
 	keywords=[
-		'console',
-		'terminal',
-		'colors',
-		'stdin'
+		"console",
+		"terminal",
+		"colors",
+		"stdin"
 		],
 	packages=[
-		'jk_console'
+		"jk_console"
 		],
 	install_requires=[
 	],
 	include_package_data=True,
 	classifiers=[
-		'Development Status :: 4 - Beta',
-		'Programming Language :: Python :: 3',
-		'License :: OSI Approved :: Apache Software License'
+		"Development Status :: 4 - Beta",
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: Apache Software License"
 	],
 	long_description=readme(),
 	zip_safe=False)
